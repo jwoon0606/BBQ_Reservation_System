7a3b8c8b48339879e41b6a60bdc39f1759841758
jwoon0606 <jwoon0606@gmail.com>
2024-06-08 02:08:20 +0900

    Create new BBQ Program

---
+++ new_src/Main.java
@@ -0,0 +1,29 @@
+/*
+client version
+-- 구현 완료 --
+1. 제목
+2. 이용 안내문
+-- 미완성 --
+3. 달력
+4. 날짜별 타임테이블
+5. 예약 시스템 구축
+6. 파일 I/O
+7. master version 만들기
+
+- 굳이 다시 만든 이유
+1. 이전 version 너무 복잡
+2. 클래스 간의 명확성 확보
+3. 프로그램 완성도 향상
+4. 장기적으로 시간 절약
+
+- 컴파일 방법
+cd new_src
+javac ./*.java
+java Main
+ */
+public class Main{
+    public static void main(String[] args){
+        CalendarFrame frame = new CalendarFrame();
+    }
+}
+
