adae9ec1ecf281fd2363382c87a33c476d4eee32
Glen02lee <155299074+Glen02lee@users.noreply.github.com>
2024-06-09 17:47:14 +0900

    Create n_Reservation.java

---
+++ src/n_Reservation.java
@@ -0,0 +1,40 @@
+public class Reservation {
+    private String name;
+    private String phone;
+    private String seat;
+    private String date;
+    private String time;
+
+    public Reservation(String name, String phone, String seat, String date, String time) {
+        this.name = name;
+        this.phone = phone;
+        this.seat = seat;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10s,%-14s,%-6s,%-9s,%-8s", name, phone, seat, date, time);
+    }
+}
